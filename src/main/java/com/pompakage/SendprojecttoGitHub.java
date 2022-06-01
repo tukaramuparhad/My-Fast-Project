812074281c061fef606958d8c92473dee67c742b
AVNI <AVNI@DESKTOP-9OO2KNR>
2022-06-01 10:58:04 +0530

    MyFastproject

---
+++ src/main/java/com/pompakage/SendprojecttoGitHub.java
@@ -0,0 +1,13 @@
+package com.pompakage;
+
+import org.testng.annotations.Test;
+
+public class SendprojecttoGitHub
+{
+	@Test
+	public void method() 
+	{
+	System.out.println("send the massage");	
+	}
+
+}
